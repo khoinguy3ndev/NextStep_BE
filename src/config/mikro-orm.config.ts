b1be12048483d77b8871f4cad0177dc6af48ceb2
Nguyen Khoi <khoibene@gmail.com>
2026-03-13 20:57:55 +0700

    run db

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -1,17 +1,17 @@
-import { Options } from '@mikro-orm/core';
-import { Migrator } from '@mikro-orm/migrations';
+import { Options } from '@mikro-orm/core'
+import { Migrator } from '@mikro-orm/migrations'
+import { PostgreSqlDriver } from '@mikro-orm/postgresql'
+import dotenv from 'dotenv'
 
-import { PostgreSqlDriver } from '@mikro-orm/postgresql';
-import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config()
 
 const mikroOrmConfig: Options<PostgreSqlDriver> = {
   driver: PostgreSqlDriver,
-  host: 'localhost',
-  port: 5432,
-  user: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  dbName: process.env.DB_USERNAME,
+  dbName: process.env.DB_NAME,
   entities: ['dist/entities'],
   entitiesTs: ['src/entities'],
   extensions: [Migrator],
@@ -20,6 +20,6 @@ const mikroOrmConfig: Options<PostgreSqlDriver> = {
     path: 'dist/migrations',
     pathTs: 'src/migrations',
   },
-};
+}
 
-export default mikroOrmConfig;
+export default mikroOrmConfig
